877ae842c03b7f88d6aa715349c624b84ffcafc3
arunwithasmile <arunwithasmile.gmail.com>
2026-07-02 01:10:06 +0530

    The Waiting Room Sound Panel

---
+++ wr-sounds/main.js
@@ -0,0 +1,30 @@
+var sounds = {};
+
+document.querySelectorAll('pad').forEach(pad => {
+    pad.addEventListener('click', () => {
+        let id = pad.getAttribute('id');
+        if (sounds[id]?.playing) {
+            sounds[id].audio.pause();
+            if (id === 'blink') {
+                sounds[id].audio.currentTime = 0;
+            }
+            sounds[id].playing = false;
+            pad.classList.remove('active');
+        } else {
+            if (!sounds[id]?.audio) {
+                const sound = id;
+                const audio = new Audio(`audio/${sound}.mp3`);
+                const loop = id !== 'truck' && id !== 'song';
+                sounds[id] = { audio, loop };
+            }
+            sounds[id].playing = true;
+            sounds[id].audio.play();
+            pad.classList.add('active');
+        }
+
+        sounds[id].audio.addEventListener('ended', () => {
+            sounds[id].playing = false;
+            pad.classList.remove('active');
+        });
+    });
+});
